2c6069e5617ff990e701eed760b1c6cbddb95041
Andre Moreira <andre.moreira.9@hotmail.com>
2021-02-17 11:15:04 +0000

    ImAdd information about operations to the user

--- src/Application.java
+++ src/Application.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose the operation: (sum or mul)");
         String operation = scanner.nextLine();
 
+        System.out.println("Please input first number: ");
         int a = scanner.nextInt();
+        System.out.println("Please input second number: ");
         int b = scanner.nextInt();
 
         int result = 0;
