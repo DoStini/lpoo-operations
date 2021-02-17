7082d9e26118083140309f079c0b4b8bfa233bc5
Nuno Alves <nyansword@gmail.com>
2021-02-17 11:06:34 +0000

    Add: Multiply and Sum operations

--- src/Application.java
+++ src/Application.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        int result = 0;
+
+        if(operation.equals("sum")) {
+            result = a + b;
+        } else if (operation.equals("mul")) {
+            result = a*b;
+        }
+
+
+
+        System.out.println(result);
     }
 }
